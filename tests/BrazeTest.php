6321f208de55a1a55881fbaf3bb79549f81e4eac
Rick West <rickwestdev@gmail.com>
2023-07-26 09:40:30 +0100

    Add braze test

--- tests/BrazeTest.php
+++ tests/BrazeTest.php
@@ -2,15 +2,27 @@
 
 namespace TutoraUK\Braze\Tests;
 
+use Illuminate\Support\Facades\Config;
+use ImmobiliareLabs\BrazeSDK\Region;
+use TutoraUK\Braze\Braze;
+
 class BrazeTest extends TestCase
 {
     public function setUp(): void
     {
         parent::setUp();
+
+        Config::set('braze.api_key', 'abc123');
+        Config::set('braze.rest_endpoint', Region::EU02);
     }
 
-    public function testNothingButTheTruth(): void
+    public function testFacadeReturnsInstanceOfBraze(): void
     {
-        $this->assertTrue(true);
+        $this->assertInstanceOf(Braze::class, \TutoraUK\Braze\Facades\Braze::getFacadeRoot());
+    }
+
+    public function testHelperReturnsInstanceOfBraze(): void
+    {
+        $this->assertInstanceOf(Braze::class, braze());
     }
 }
